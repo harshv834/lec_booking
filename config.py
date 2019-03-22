1e94595eb24ceb65f70abb72538d02ae8eae7303
harshv834 <harshv834@gmail.com>
2019-03-23 03:17:43 +0530

    heroku deployment

--- config.py
+++ config.py
@@ -6,3 +6,4 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
             'sqlite:///' + os.path.join(basedir,'app,db')
     SQLACHEMY_TRACK_MODIFICATIONS = False
+    LOG_TO_STDOUT = os.environ.get("LOG_TO_STDOUT")
